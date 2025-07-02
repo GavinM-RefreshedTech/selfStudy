a9f314794232baaa1ea938e71bbf7a0e34ba251c
GavinM-RefreshedTech <gavin.mccormack@refreshedtech.com>
2025-07-02 12:41:28 -0400

    fastpractice so far

---
+++ app/schemas.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+#This handles the user sending data to us
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True #defaults to true if not provided
+
+class PostCreate(PostBase):
+    pass
+
+
+# data user gets back on account creation
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    class Config:
+            orm_mode=True
+
+# Reponse to User
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+    class Config:
+        orm_mode = True
+
+
+
+# data user sends
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+     email: EmailStr
+     password: str
+
+
+class Token(BaseModel):
+     access_token: str
+     token_type: str
+
+class TokenData(BaseModel):
+     id: Optional[str] = None
